6c6a5a430aba6c471082429f19a09fce615d030e
bassamxyz <beaxyzxyzxyz@gmail.com>
2025-12-20 15:43:43 +0300

    Delete unnecessary files

--- src/components/SharedComponent/HeroSub/index.tsx
+++ src/components/SharedComponent/HeroSub/index.tsx
@@ -7,13 +7,11 @@ interface HeroSubProps {
 const HeroSub: FC<HeroSubProps> = ({ title }) => {
 
     return (
-        <>
-            <section className="py-40 bg-herosub-bg bg-no-repeat bg-cover lg:mt-40 sm:mt-44 mt-20">
-                <div className="container mx-auto lg:max-w-screen-xl px-4">
-                    <h2 className="text-white md:text-56 text-36 font-medium">{title}</h2>
-                </div>
-            </section>
-        </>
+        <section className="py-40 bg-herosub-bg bg-no-repeat bg-cover lg:mt-40 sm:mt-44 mt-20">
+            <div className="container mx-auto lg:max-w-screen-xl px-4">
+                <h2 className="text-white md:text-56 text-36 font-medium">{title}</h2>
+            </div>
+        </section>
     );
 };
 
